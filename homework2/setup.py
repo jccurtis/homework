e90f181981b89b44081f6e0b7a9805ff06883da7
jccurtis <joseph.c.curtis@gmail.com>
2013-09-18 16:29:22 -0700

    Added setup.py installation file

---
+++ homework2/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='CalCalc',
+	  description='AY250 Homework 2 for Wolfram Alpha calculator',
+      author='Joseph Curtis',
+      author_email='jccurtis@berkeley.edu',
+      version='1.0',
+      py_modules=['CalCalc'],
+      )
