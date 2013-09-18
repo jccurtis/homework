c6a78042b4dbecd67b4aa73b355982f6741246fb
jccurtis <joseph.c.curtis@gmail.com>
2013-09-18 12:44:16 -0700

    Made eval safer.
    Added empty set of global variables to pass to eval and prevented
    evaluation with global variables with underscores.

--- homework2/CalCalc.py
+++ homework2/CalCalc.py
@@ -6,7 +6,10 @@ def calculate(INPUT):
 
 	assert type(INPUT) is StringType, "You must input a string."
 
+	if INPUT.count('_') != 0:
+		raise ValueError('Cannot evaluate input with underscores (_), it is not safe')
+
 	try:
-		return eval(INPUT)
+		return eval(INPUT, {})
 	except Exception, e:
 		print 'Python cannot evaluate, using Wolfram Alpha'
