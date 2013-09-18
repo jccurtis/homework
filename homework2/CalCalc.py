146e756c80294bc0871176ca6b6ef9936ce3cf3d
jccurtis <joseph.c.curtis@gmail.com>
2013-09-18 16:59:16 -0700

    Final commit of CalCalc.py

--- homework2/CalCalc.py
+++ homework2/CalCalc.py
@@ -18,16 +18,32 @@ def calculate(INPUT, return_float=False, use_wolf=False):
 		OUTPUT = eval(INPUT, {})
 	except Exception, e:
 		print 'Python cannot evaluate, using Wolfram Alpha...'
-		URL = 'http://api.wolframalpha.com/v2/query?input=' + INPUT.replace(' ', '%20') + '&appid=UAGAWR-3X6Y8W777Q'
+		URL = 'http://api.wolframalpha.com/v2/query?input=' + INPUT.replace(' ', '%20') + '&appid=TG4537-XXVU2L7PK6'
 		print 'URL: ' + URL
 		response = urllib2.urlopen(URL)               #xml code from wolfram alpha
 		parsed_response = fromstring(response.read()) #convert xml to tree structure
 		OUTPUT = parsed_response[1][0][0].text        #takes the value of the 2nd child, 1st subchild, 1st subsubchild
+		#decodedOUTPUT = OUTPUT.encode('utf8')         #decodes to UTF8
 
 	if return_float == True:
 		return OUTPUT
 	else:
 		print OUTPUT
+
+def test_1():
+	assert abs(4. - calculate('2**2',return_float=True)) < .001
+
+def test_2():
+	assert (15. - calculate('5+10',return_float=True)) < .001
+
+def test_3():
+	assert abs(18. - calculate('9*2',return_float=True)) < .001
+
+def test_4():
+	assert abs(1. - calculate('5./5.',return_float=True)) < .001
+
+def test_5():
+	assert abs(64. - calculate('8**2',return_float=True)) < .001
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Wolfram Alpha String Calculator')
