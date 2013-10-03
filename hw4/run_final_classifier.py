0fea559ec2767b67a4db75de13a51ce38f077bf3
jccurtis <joseph.c.curtis@gmail.com>
2013-10-02 22:44:36 -0700

    Updated to use npy stored trained_classifier and imported functions

--- hw4/run_final_classifier.py
+++ hw4/run_final_classifier.py
@@ -1,4 +1,4 @@
-def run_final_classifier(path,forest='trained_classifier.p'):
+def run_final_classifier(path,forest='classifier.npy'):
 	'''
 	Run Final Classifier uses the random forest classifier called 
 	trained_classifier I built with the full 4244 image training set. 
@@ -7,15 +7,12 @@ def run_final_classifier(path,forest='trained_classifier.p'):
 	'''
 
 	from os import listdir
-
-
+	from functions import split_seq, extract_features
 
 	print 'filename          predicted_class'
 	print '------------------------------------------'
 
 	for im_name in listdir(path):
-
-
 
 
 	return None
