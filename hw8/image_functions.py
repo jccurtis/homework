73539b949de0dfbf31f0ac25a50ea3487857c3c0
jccurtis <joseph.c.curtis@gmail.com>
2013-10-30 23:51:09 -0700

    change image_search to image_functions and added vignette

--- hw8/image_functions.py
+++ hw8/image_functions.py
@@ -1,4 +1,3 @@
-
 import os
 import urllib
 import urllib2
@@ -9,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-def scrapeImage(query):
+def scrape_image(query):
     '''
     Input:  query - string passed to Google image search Ajax api. 
                     Google returns 8 image results in JSON format
@@ -43,10 +42,52 @@ def scrapeImage(query):
         url = result['unescapedUrl']
         counter += 1
         try:
-            urllib.urlretrieve(url,'test.jpg')
+            urllib.urlretrieve(url,fName)
             break
         except:
             print counter,"image(s) didn't download correctly"
     # Load the image
     imArr = plt.imread(fName)
     return url,fName,imArr
+
+def vignette(imArr):
+    '''
+    Performs vignetting to the image using a mask (with the center bright) 
+    for pixel burning using the magnitude of the Euclidian distance from 
+    the center of the image as the mask value. The mask is then normalized
+    and multiplied by each color channel (only one if greyscale).
+    
+    Input:  imArr - Numpy image array
+    
+    Output: imArrMasked - imArr with vignetting
+    '''
+    pxHeight = imArr.shape[0]
+    pxWidth  = imArr.shape[1]
+    pxDepth  = len(imArr.shape)   #1 for grayscale, 3 for RGB
+    #center of image pixel array
+    pxHeightHalf = pxHeight/2
+    pxWidthHalf = pxWidth/2
+    #build mask (center dark) for pixel burning using the magnitude of 
+    #Euclidian distance from the center of the image. I had to be 
+    #careful of data types here becuase I got overflow with int8's.
+    #Thus I set the Mask dtype to float.
+    imMask = np.zeros_like(imArr, dtype=np.float)
+    for y in range(pxHeight):
+        for x in range(pxWidth):
+            px = np.sqrt((pxHeightHalf-y)**2+(pxWidthHalf-x)**2)
+            if pxDepth == 3:
+                imMask[y,x,0] = px
+                imMask[y,x,1] = px
+                imMask[y,x,2] = px
+            else:
+                imMask[y,x] = px
+    #inverse mask (center bright) and normalize
+    imMaskNorm = 1-imMask/imMask.max()
+    imArrMasked = imArr.copy()
+    #apply mask to every layer
+    if pxDepth == 3:
+        for i in range(3):
+            imArrMasked[...,i] = imArr[...,i]*imMaskNorm[...,i]
+    else:
+        imArrMasked = imArr*imMaskNorm
+    return imArrMasked
